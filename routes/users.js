856c349dfd1a97c88d42f4b07fd6a71a316aa9b0
sharmaaman4419 <sharmaaman4419@gmail.com>
2019-09-14 22:41:40 +0530

    Update Changes

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,17 @@ var express = require('express');
 var router = express.Router();
 // Requring The Model Of Schema
 var Book = require("../Models/Books");
+
 // Handling The Post Request
 router.post("/create",(req,res) => {
   Book.create(req.body,(err,Added) => {
     if(err) return next(err);
-    res.json({message:"Book added Sucessfully"})
+    res.json({message:"Book added Sucessfully"});
   });
 });
 
 // Handling The Get Request
-router.get("/Read",(req,res,next) => {
+router.get("/read",(req,res,next) => {
   Book.find({},(err,Books) => {
     if(err) return next(err);
     res.json({Books});
@@ -19,17 +20,18 @@ router.get("/Read",(req,res,next) => {
 });
 
 // Handling The Update Request
-router.put("/Update/:id",(req,res,next) => {
+router.put("/update/:id",(req,res,next) => {
   var id = req.params.id;
-  Book.findByIdAndUpdate(id,(err,Updated) => {
+  Book.findByIdAndUpdate(id, (err,Updated) => {
     if(err) return next(err);
     res.json({message: "Updated SucessFully"});
   });
 });
 
 // Handling The Delete Route
-router.delete("Delete/:id",(req,res,next) => {
-  Book.findByIdAndDelete(req.params.id,(err,Deleted) => {
+router.delete("delete/:id",(req,res,next) => {
+  var id = req.params.id;
+  Book.findOneAndDelete(id, (err,Deleted) => {
     if(err) return next(err);
     res.json({message: "Deleted SucessFully"});
   });
